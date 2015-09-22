5593d9c0c48fee2204dc7c413f34fb98e8a23a25
Timothyl <timothyjlau@gmail.com>
2015-09-22 10:53:55 -0400

    All the tests!

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :food_truck
   belongs_to :user
-
   validates :header, presence: true
   validates :body, presence: true
+  validates :body, length: { minimum: 30 }
   validates :rating, presence: true, numericality: { only_integer: true }
   validates :rating, inclusion: { in: 1..5 }
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,10 +1,23 @@
-require 'factory_girl'
+require "factory_girl"
 
 FactoryGirl.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
-    password 'password'
-    password_confirmation 'password'
+    password "password"
+    password_confirmation "password"
+  end
+
+  factory :food_truck do
+    sequence(:name) {|n| "foodtruck#{n}" }
+    description "This is the description for this food truck"
+    avg_rating 3
+    location "On the street"
+  end
+
+  factory :review do
+    sequence(:header) {|n| "review#{n}"}
+    body "This is the body of the review. It's terribly interesting."
+    rating 3
   end
 
 end
