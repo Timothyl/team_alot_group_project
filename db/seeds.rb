61459d9ebeeaf73564b14b94f4fd9d7c36cef13c
Timothyl <timothyjlau@gmail.com>
2015-09-23 11:50:49 -0400

    Added review pagination with ajax

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,30 @@ foodtrucks = [
       cuisine made
     from locally sourced and sustainable ingredients.", 4, "Fenway"]
 ]
+
+
 foodtrucks.each do |foodtruck|
   name, description, avg_rating, location = foodtruck
-  FoodTruck.find_or_create_by!(name: name, description: description,
+  f = FoodTruck.find_or_create_by!(name: name, description: description,
     avg_rating: avg_rating, location: location)
+  Review.find_or_create_by!(header: "This is a review header",
+   body: "This is a review body.
+   I am typing this because we need a minimum charcter count", rating: 3,
+   food_truck: f)
+  Review.find_or_create_by!(header: "2This is a review header",
+   body: "2This is a review body.
+   I am typing this because we need a minimum charcter count", rating: 3,
+   food_truck: f)
+  Review.find_or_create_by!(header: "3This is a review header",
+   body: "3This is a review body.
+   I am typing this because we need a minimum charcter count", rating: 3,
+   food_truck: f)
+  Review.find_or_create_by!(header: "4This is a review header",
+   body: "4This is a review body.
+   I am typing this because we need a minimum charcter count", rating: 3,
+   food_truck: f)
+   Review.find_or_create_by!(header: "5This is a review header",
+    body: "5This is a review body.
+    I am typing this because we need a minimum charcter count", rating: 3,
+    food_truck: f)
 end
