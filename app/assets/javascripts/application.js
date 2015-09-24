0513cd8d7e74fc9fd90ae3d4cbb3703e40846a51
philiptang42 <philiptang42@gmail.com>
2015-09-24 10:14:31 -0400

    add food truck review form and tests and inline js gone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,9 @@ $(function(){ $(document).foundation(); });
 function toggleVisibility(id) {
   var e = document.getElementById(id);
   if(e.style.display == 'block')
+  {
      e.style.display = 'none';
   else
      e.style.display = 'block';
+   }
 }
