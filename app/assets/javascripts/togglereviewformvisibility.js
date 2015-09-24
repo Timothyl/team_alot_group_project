78a9483d9db23cbe41065e6dbf44ce49b13cbf03
Timothyl <timothyjlau@gmail.com>
2015-09-24 17:16:14 -0400

    started work on editing reviews

--- app/assets/javascripts/togglereviewformvisibility.js
+++ app/assets/javascripts/togglereviewformvisibility.js
@@ -1,11 +1,16 @@
-function toggleVisibility(id) {
-  var e = document.getElementById(id);
-  if(e.style.display == 'block')
-  {
-     e.style.display = 'none';
-   }
-  else
-  {
-     e.style.display = 'block';
-   }
-}
+$(function(){
+  $(".button-edit-form").on("click", function(){
+    var $panel = $(this).closest(".panel");
+    var $form = $panel.find(".form-div");
+    $form.toggleClass("hide");
+  });
+});
+
+$(function(){
+  $(".button-new-form").on("click", function(){
+    debugger;
+    var $div = $(this).closest(".new-review");
+    var $form = $div.find(".reviewform");
+    $form.toggleClass("hide");
+  });
+});
