1eda74381eccb6c396433f0df616c4fed490dbf5
Timothyl <timothyjlau@gmail.com>
2015-09-28 12:39:30 -0400

    testing for editing reviews w/ JS

--- spec/features/review_tests/user_edits_review_spec.rb
+++ spec/features/review_tests/user_edits_review_spec.rb
@@ -9,20 +9,28 @@ feature "user edits a food truck review", %{
   # If a user is logged in, they can edit their own reviews.
 
   scenario "user is logged in and edits their review", js: true do
-    food_truck_review = FactoryGirl.create(:review)
-    visit food_truck_path(food_truck_review.food_truck)
-    click_button "Edit your review"
+    user = FactoryGirl.create(:user)
+    food_truck_review = FactoryGirl.create(:review, user: user)
 
+
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    click_link "foodtruck1"
+
+    click_button "Edit your review"
     fill_in "Header", with: "This is the test header!"
-    fill_in "Body", with: "This is the test body. It's perfect for testing
-    and it is the perfect length for it."
+    fill_in "Body", with: "This is the test body. It's perfect for testing and it is the perfect length for it."
     fill_in "Rating", with: 3
 
     click_button "Submit Review"
 
     expect(page).to have_content("This is the test header!")
-    expect(page).to have_content("This is the test body. It's perfect for testing
-    and it is the perfect length for it.")
-    expect(page).to have_content("3")
+    expect(page).to have_content("This is the test body. It's perfect for testing and it is the perfect length for it.")
+    value = expect(page).to have_content("3")
+    binding.pry
   end
 end
