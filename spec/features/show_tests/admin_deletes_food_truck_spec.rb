419049698ca3c154a3f879f812b73ff71eaac78e
Timothyl <timothyjlau@gmail.com>
2015-09-28 14:54:40 -0400

    hound violation

--- spec/features/show_tests/admin_deletes_food_truck_spec.rb
+++ spec/features/show_tests/admin_deletes_food_truck_spec.rb
@@ -7,22 +7,22 @@ feature "admin deletes a food truck", %{
 } do
 
   scenario "admin deletes a food truck" do
-  admin = FactoryGirl.create(:user, admin: true)
-  food_truck = FactoryGirl.create(:food_truck)
+    admin = FactoryGirl.create(:user, admin: true)
+    food_truck = FactoryGirl.create(:food_truck)
 
-  visit "/"
-  click_link('Sign In')
+    visit "/"
+    click_link('Sign In')
 
-  fill_in 'Email', with: admin.email
-  fill_in 'Password', with: admin.password
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
 
-  click_button 'Log in'
+    click_button 'Log in'
 
-  visit food_truck_path(food_truck)
+    visit food_truck_path(food_truck)
 
-  click_link "Delete food truck"
+    click_link "Delete food truck"
 
-  expect(page).to_not have_content(food_truck.name)
+    expect(page).to_not have_content(food_truck.name)
   end
 
 end
