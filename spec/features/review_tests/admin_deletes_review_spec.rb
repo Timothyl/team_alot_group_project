54feb96e68375262ba15a49549c89f0ade5e9682
Timothyl <timothyjlau@gmail.com>
2015-09-29 10:18:04 -0400

    Saving work

---
+++ spec/features/review_tests/admin_deletes_review_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+feature "Admin deletes review from food truck", %{
+  As a authenticated administrator
+  I want to delete a review from a food truck
+  So that I can remove inappropriate content
+} do
+  # Acceptance Criteria
+  # If a review is deemed inappropriate, the admin can
+  # delete the review.
+
+  scenario "admin deletes review from food truck" do
+    
+  end
+end
