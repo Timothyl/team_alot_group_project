6f71cfa6bff20fc64dd6b142560db886262cb75e
Timothyl <timothyjlau@gmail.com>
2015-09-29 15:41:07 -0400

    hound violations

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',                        # required
-    aws_access_key_id: ENV["AWS_KEY_ID"],            # required
-    aws_secret_access_key: ENV["AWS_KEY"],               # required
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_KEY"],
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]
 end
