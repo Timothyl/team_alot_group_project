37a3ac102312f11f0909db502d65ff7b45fb9ebc
Matthew O'Hara <matthewohara@Matthews-MacBook-Air.local>
2015-09-30 09:01:17 -0400

    fixed hound js errors

--- app/assets/javascripts/upvote.js
+++ app/assets/javascripts/upvote.js
@@ -1,20 +1,19 @@
 $(".vote").click(function()
 {
-
-  var voteType = this.id
-  var parentId = this.parentElement
-  var reviewId = parentId.parentElement.id
+  var voteType = this.id;
+  var parentId = this.parentElement;
+  var reviewId = parentId.parentElement.id;
   var url = window.location.pathname;
-  var foodTruckId = url.substring(url.lastIndexOf('/') + 1);
+  var foodTruckId = url.substring(url.lastIndexOf("/") + 1);
   $.ajax({
   METHOD: "POST",
   url: "/food_trucks/" + foodTruckId + "/votes",
   dataType: "JSON",
-  data: { _method:"put", review_id: reviewId, vote_type: voteType, food_truck_id: foodTruckId },
+  data: { _method:"put", review_id: reviewId, vote_type: voteType,
+         food_truck_id: foodTruckId },
   success: function(data)
   {
-    var panel = $("panel").find(parentId.id)
     $(parentId).children("#review_score").html(data);
   },
-})
+});
 });
