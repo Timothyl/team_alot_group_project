d9bc569516e03560eaf3eb7fc0ff3704c8df1466
Matthew O'Hara <matthewohara@Matthews-MacBook-Air.local>
2015-09-30 15:23:07 -0400

    refactored votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -10,49 +10,47 @@ class VotesController < ApplicationController
   def create
     @food_truck = FoodTruck.find(params[:food_truck_id])
     @review = @food_truck.reviews.where(id: params[:review_id])
-    vote_check = Vote.find_by(review_id: params[:review_id],user_id: current_user)
-    if vote_check.nil?
-      @vote = Vote.new(review_id: params[:review_id],user_id: current_user.id)
-    else
+    vote_check = Vote.find_by(review_id: params[:review_id],
+                              user_id: current_user)
 
-      if params[:vote_type] == "upvote"
-        if vote_check.upvote == 1
-          vote_check.destroy
-          @vote = nil
-        else
-          @vote = vote_check
-        end
-      else
-        if vote_check.downvote == 1
-          vote_check.destroy
-          @vote = nil
-        else
-          @vote = vote_check
-        end
-      end
+    if vote_check.nil?
+      @vote = Vote.new(review_id: params[:review_id], user_id: current_user.id)
+    else
+      @vote = vote_check.destroy_if_exists(params[:vote_type])
     end
 
+  #   if !@vote.nil?
+  #     if params[:vote_type] == "upvote"
+  #       @vote.upvote = 1
+  #       @vote.downvote = 0
+  #     elsif params[:vote_type] == "downvote"
+  #       @vote.downvote = 1
+  #       @vote.upvote = 0
+  #     end
+  #     respond_to do |format|
+  #       if @vote.save
+  #         check_review = Review.find(params[:review_id])
+  #         format.html
+  #         format.json { render json: check_review.score }
+  #       end
+  #     end
+  #   else
+  #     respond_to do |format|
+  #       check_review = Review.find(params[:review_id])
+  #       format.html
+  #       format.json { render json: check_review.score }
+  #     end
+  #   end
+  # end
     if !@vote.nil?
-      if params[:vote_type] == "upvote"
-        @vote.upvote = 1
-        @vote.downvote = 0
-      elsif params[:vote_type] == "downvote"
-        @vote.downvote = 1
-        @vote.upvote = 0
-      end
-      respond_to do |format|
-        if @vote.save
-          check_review = Review.find(params[:review_id])
-          format.html
-          format.json { render json: check_review.score }
-        end
-      end
-    else
-      respond_to do |format|
-        check_review = Review.find(params[:review_id])
-        format.html
-        format.json { render json: check_review.score }
-      end
+      @vote.change_vote(params[:vote_type])
+      @vote.save
+    end
+
+    respond_to do |format|
+      check_review = Review.find(params[:review_id])
+      format.html
+      format.json { render json: check_review.score }
     end
   end
 end
--- spec/features/vote_tests/user_votes_on_review_spec.rb
+++ spec/features/vote_tests/user_votes_on_review_spec.rb
@@ -10,11 +10,8 @@ feature "user upvotes a food truck review", %{
 
   scenario "user is NOT logged in and tries voting on a review", js: true do
     food_truck_review = FactoryGirl.create(:review)
-
     visit food_truck_path(food_truck_review.food_truck)
-
     page.find("#novoteforyou").click
-
     expect(page).to have_content("Log in")
     expect(page).to have_content("Forgot your password?")
     expect(page).to_not have_content("Score: 1")
@@ -23,13 +20,10 @@ feature "user upvotes a food truck review", %{
   scenario "user is logged in, can see a review to vote on", js: true do
     user = FactoryGirl.create(:user)
     food_truck_review = FactoryGirl.create(:review, user: user)
-
     visit new_user_session_path
-
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
-
     visit food_truck_path(food_truck_review.food_truck)
     expect(page).to have_content(food_truck_review.body)
   end
@@ -38,74 +32,32 @@ feature "user upvotes a food truck review", %{
     before :each do
       user = FactoryGirl.create(:user)
       food_truck_review = FactoryGirl.create(:review, user: user)
-
       visit new_user_session_path
-
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
       click_button "Log in"
-
       visit food_truck_path(food_truck_review.food_truck)
-
     end
 
     scenario "user is logged in and upvotes a review", js: true do
-      # user = FactoryGirl.create(:user)
-      # food_truck_review = FactoryGirl.create(:review, user: user)
-      #
-      # visit new_user_session_path
-      #
-      # fill_in "Email", with: user.email
-      # fill_in "Password", with: user.password
-      # click_button "Log in"
-
-      # visit food_truck_path(food_truck_review.food_truck)
-
       page.find("#upvote").click
-
       expect(page).to have_content("1")
     end
 
     scenario "user is logged in and downvotes a review", js: true do
-      # user = FactoryGirl.create(:user)
-      # food_truck_review = FactoryGirl.create(:review, user: user)
-      #
-      # visit new_user_session_path
-      #
-      # fill_in "Email", with: user.email
-      # fill_in "Password", with: user.password
-      # click_button "Log in"
-
-      # visit food_truck_path(food_truck_review.food_truck)
-
       page.find("#downvote").click
-
       expect(page).to have_content("-1")
     end
 
     scenario "user regrets their decision, changes vote", js: true do
-      # user = FactoryGirl.create(:user)
-      # food_truck_review = FactoryGirl.create(:review, user: user)
-      #
-      # visit new_user_session_path
-      #
-      # fill_in "Email", with: user.email
-      # fill_in "Password", with: user.password
-      # click_button "Log in"
-
-      # visit food_truck_path(food_truck_review.food_truck)
-
       page.find("#upvote").click
       page.find("#downvote").click
-
       expect(page).to have_content("-1")
     end
 
     scenario "indecisive user undoes their vote", js: true do
-
       page.find("#upvote").click
       page.find("#upvote").click
-
       expect(page).to have_content("0")
     end
   end
