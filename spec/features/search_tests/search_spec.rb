4e8e3f1121436511b56d787fd68a5a230ab50a45
Matthew O'Hara <matthewohara@Matthews-MacBook-Air.local>
2015-10-01 16:04:12 -0400

    Broken tests with nav bar

--- spec/features/search_tests/search_spec.rb
+++ spec/features/search_tests/search_spec.rb
@@ -9,18 +9,24 @@ feature "user searches for food truck", %{
   # * If a user searches for a food truck, they should see search results.
   scenario "user finds the food truck" do
     truck = FactoryGirl.create(:food_truck)
+    truck2 = FactoryGirl.create(:food_truck)
     visit food_trucks_path
 
     fill_in "search-field", with: truck.name
-    click_button "Search"
+    click_button "search"
+    binding.pry
     expect(page).to have_content(truck.name)
+    expect(page).to_not have_content(truck2.name)
+
   end
 
   scenario "user sees error message if food truck not in database" do
+    truck = FactoryGirl.create(:food_truck)
     visit food_trucks_path
 
-    fill_in "search-field", with: "abcd"
+    fill_in "search-field", with: "wjdoiajwaoijdawoidjawoi"
     click_button "Search"
+    save_and_open_page
     expect(page).to have_content("No food trucks found")
   end
 end
