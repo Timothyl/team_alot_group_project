cbd8529dd850de80984b8c76c322f2e77858b36b
philiptang42 <philiptang42@gmail.com>
2015-10-02 14:36:58 -0400

    tests passssedsadaas

--- app/controllers/food_trucks_controller.rb
+++ app/controllers/food_trucks_controller.rb
@@ -1,7 +1,8 @@
 class FoodTrucksController < ApplicationController
   def index
     if params[:search]
-      @food_trucks = FoodTruck.basic_search(params[:search]).order("created_at DESC")
+      par = "create_at DESC"
+      @food_trucks = FoodTruck.basic_search(params[:search]).order(par)
       if @food_trucks == []
         flash[:errors] = 'No food trucks found'
       else
